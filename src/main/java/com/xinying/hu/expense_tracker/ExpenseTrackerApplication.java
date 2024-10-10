8aa10925ae5a6a62a3e2358b30d5fbb00b874288
Xinying Hu <xinying0120hu@gmail.com>
2024-10-09 23:51:08 -0700

    Initial commit from Sprintboot template

---
+++ src/main/java/com/xinying/hu/expense_tracker/ExpenseTrackerApplication.java
@@ -0,0 +1,19 @@
+package com.xinying.hu.expense_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ExpenseTrackerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseTrackerApplication.class, args);
+	}
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
