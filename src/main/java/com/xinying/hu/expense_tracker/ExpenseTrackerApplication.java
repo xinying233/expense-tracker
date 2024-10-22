8eb8bffac195f028ba66da0e4379f69b9088b3d3
Xinying Hu <xinying0120hu@gmail.com>
2024-10-21 22:51:29 -0700

    application entrance

--- src/main/java/com/xinying/hu/expense_tracker/ExpenseTrackerApplication.java
+++ src/main/java/com/xinying/hu/expense_tracker/ExpenseTrackerApplication.java
@@ -2,8 +2,6 @@ package com.xinying.hu.expense_tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -11,9 +9,5 @@ import org.springframework.web.bind.annotation.RestController;
 public class ExpenseTrackerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ExpenseTrackerApplication.class, args);
-	}
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
 	}
 }
