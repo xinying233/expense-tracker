6d678d4e72248fb5abf21f17a28384685f70a7bd
Xinying Hu <xinying0120hu@gmail.com>
2024-10-21 22:51:00 -0700

    hello controller

---
+++ src/main/java/com/xinying/hu/expense_tracker/HelloController.java
@@ -0,0 +1,12 @@
+package com.xinying.hu.expense_tracker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Sprint Boot!";
+    }
+}
