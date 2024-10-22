03a424993ce3f6dcc6ea6d23a4a13604a6b55a9b
Xinying Hu <xinying0120hu@gmail.com>
2024-10-21 22:53:07 -0700

    add JPA entity and repository

---
+++ src/main/java/com/xinying/hu/expense_tracker/User.java
@@ -0,0 +1,39 @@
+package com.xinying.hu.expense_tracker;
+
+import jakarta.persistence.*;
+
+@Table(name="users")
+@Entity // This tells Hibernate to make a table out of this class
+public class User {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+
+    private String name;
+
+    private String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
