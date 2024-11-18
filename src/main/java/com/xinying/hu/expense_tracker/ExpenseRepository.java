9b8f64f3210e20626bb8cb94ddeb27d9918a837e
Xinying Hu <xinying_hu@berkeley.edu>
2024-11-18 00:07:41 -0800

    settle one expense

--- src/main/java/com/xinying/hu/expense_tracker/ExpenseRepository.java
+++ src/main/java/com/xinying/hu/expense_tracker/ExpenseRepository.java
@@ -1,9 +1,13 @@
 package com.xinying.hu.expense_tracker;
+import jakarta.annotation.Nonnull;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.lang.NonNull;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 
 
 public interface ExpenseRepository extends CrudRepository<Expense, Integer> {
@@ -11,4 +15,6 @@ public interface ExpenseRepository extends CrudRepository<Expense, Integer> {
     List<Expense> findAllByPayerId(Integer payerId);
 
     List<Expense> findAllByBorrowerId(Integer borrowerId);
+
+    Optional<Expense> findById(Integer id);
 }
--- src/main/java/com/xinying/hu/expense_tracker/UserRelationRepository.java
+++ src/main/java/com/xinying/hu/expense_tracker/UserRelationRepository.java
@@ -4,9 +4,14 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.swing.text.html.Option;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRelationRepository extends CrudRepository<UserRelation, Integer> {
     public Optional<UserRelation> findByUserAAndUserB(User userA, User userB);
+
+    List<UserRelation> findAllByUserA(User userA);
+
+    List<UserRelation> findAllByUserB(User userB);
 }
